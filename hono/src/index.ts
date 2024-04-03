8e1cdec20ecd6698273ddaaef01380afb8ca558d
Yukie M Billal <yukiembillal01@gmail.com>
2024-04-03 11:01:46 +0700

    feat: todos resources

--- hono/src/index.ts
+++ hono/src/index.ts
@@ -1,12 +1,15 @@
 import { Hono } from 'hono'
-import { getTodos } from './todos/todos.controller'
+import { createTodo, deleteTodo, getTodos, updateTodo } from './todos/todos.controller'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.json({message: "Hello world"})
-})
-
+app.get('/', (c) => c.text("Hello from bun todos api"))
 app.get('/todos', getTodos)
+app.post('/todos', createTodo)
+app.put('/todos', updateTodo)
+app.delete('/todos', deleteTodo)
 
-export default app
+export default {
+  port: 5000,
+  fetch: app.fetch
+}
